6e354359082b24bb8e804a2e9e93f6021072f048
league <league@192.168.1.135>
2026-09-12 13:00:06 -0700

    yes

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -2,17 +2,72 @@
 package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
+import java.util.Random;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-	    //2. Make the robot draw a shape (this will take more than one line of code)		
+		Robot jason = new Robot();
+		jason.setSpeed(25);
+		Random ran1 =new Random();
+		
+		
+		while(true) {
+String color =JOptionPane.showInputDialog("what color should the robot use to draw? no capitals");
+		
+		
+		if (color.equals("red")) {
+			jason.setPenColor(255,0,0);
+		}
+		else if (color.equals("green")) {
+			jason.setPenColor(0,255,0);
+		}
+		else if (color.equals("blue")) {
+			jason.setPenColor(0,0,255);
+		}
+		else if (color.equals("orange")) {
+			jason.setPenColor(255,165,0);
+		}
+		else if (color.equals("yellow")) {
+			jason.setPenColor(255,255,0);
+		}
+		else if (color.equals("purple")) {
+			jason.setPenColor(128,0,128);
+		}
+		else  {
+			jason.setPenColor(ran1.nextInt(256),ran1.nextInt(256),ran1.nextInt(256));
+		}
+		
+	    //2. Make the robot draw a shape (this will take more than one line of code)	
+		jason.setPenWidth(10);
+		jason.penDown();
+		jason.move(200);
+		jason.turn(90);
+		jason.move(200);
+		jason.turn(90);
+		jason.move(200);
+		jason.turn(90);
+		jason.move(200);
+		jason.move(200);
+		jason.turn(90);
+		jason.move(200);
+		jason.turn(90);
+		jason.move(200);
+		jason.turn(90);
+		jason.move(200);
+		jason.turn(90);
+		}
 		//3. Set the pen width to 10
+	
 		//4. Ask the user what color pen they would like the robot to draw with
-		//5. Use an if/else statement to set the pen color that the user requested
-        //6. If the user doesn't enter anything, choose a random color
+		
+        //5. Use an if/else statement to set the pen color that the user requested
+		//6. If the user doesn't enter anything, choose a random color
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 
--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,12 +3,25 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user how many cats they have
+		String cats =JOptionPane.showInputDialog("how many cats do you have? number answer ex- 1,2,3,4 ect");
 		// 2. Convert their answer into an int
+		int kats = Integer.parseInt(cats);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+		if (kats>=(3)){
+			JOptionPane.showMessageDialog(null,"you are a crazy cat lady");
+		}
+		else if (kats<3&&(kats>0)){
+			JOptionPane.showMessageDialog(null,"you are a crazy cat lady");
+		}
+		else {
+			playVideo("https://www.youtube.com/watch?v=VLVdjLbXdm4");
+		}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 		//    Hint: You can use the && operator 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
